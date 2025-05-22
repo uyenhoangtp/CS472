fe9b2b602fb791bef6d383661d583290aec6fd71
uyenhoang <uyenhtp@gmail.com>
2025-05-22 18:01:52 -0500

    hotfix

--- src/finalProject/client/src/api/reviews.ts
+++ src/finalProject/client/src/api/reviews.ts
@@ -1,7 +1,6 @@
 import type { Review } from '../types/types';
 
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-
 export async function getReviews(productId: number): Promise<Review[]> {
   const res = await fetch(`${BASE_URL}/products/${productId}/reviews`);
   if (!res.ok) throw new Error('Failed to fetch reviews');
